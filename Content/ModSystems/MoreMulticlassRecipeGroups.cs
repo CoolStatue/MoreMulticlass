5679fed86a642ab6d9da22e29f3191b301f8301a
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2025-11-21 19:02:14 +0800

    Added goldbar recipe group and gladiator armor crafting

---
+++ Content/ModSystems/MoreMulticlassRecipeGroups.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using MoreMulticlass.Content.Items.Miscellaneous;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MoreMulticlass.ModSystems
+{
+    public class MoreMulticlassRecipeGroups : ModSystem
+    {
+        public override void AddRecipeGroups()
+		{
+			RecipeGroup group = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.GoldBar)}", ItemID.PlatinumBar, ItemID.GoldBar);
+			RecipeGroup.RegisterGroup(nameof(ItemID.GoldBar), group);
+		}
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = Recipe.Create(ItemID.GladiatorLeggings, 1);
+            recipe.AddRecipeGroup(nameof(ItemID.GoldBar), 16);
+            recipe.AddIngredient(ModContent.ItemType<HopliteArmorShard>(), 6);
+            recipe.Register();
+
+            recipe = Recipe.Create(ItemID.GladiatorBreastplate, 1);
+            recipe.AddRecipeGroup(nameof(ItemID.GoldBar), 20);
+            recipe.AddIngredient(ModContent.ItemType<HopliteArmorShard>(), 9);
+            recipe.Register();
+            
+            recipe = Recipe.Create(ItemID.GladiatorHelmet, 1);
+            recipe.AddRecipeGroup(nameof(ItemID.GoldBar), 14);
+            recipe.AddIngredient(ModContent.ItemType<HopliteArmorShard>(), 5);
+            recipe.Register();
+        }
+
+    }
+}
