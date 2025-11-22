2de9acfa067fb8b81b14a4500d01bcbda1464bc7
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2025-11-22 11:50:37 +0800

    PalladiumBreastplateOverride functionality

--- Content/Items/Armor/PalladiumArmorSet/PalladiumBreastplateOverride.cs
+++ Content/Items/Armor/PalladiumArmorSet/PalladiumBreastplateOverride.cs
@@ -14,6 +14,26 @@ namespace MoreMulticlass.Content.Items.Armor.PalladiumArmorSet
         public override bool AppliesToEntity(Item entity, bool lateInstantiation)
             => entity.type == ItemID.PalladiumBreastplate; 
         public static readonly int MaxHealthIncrease = 40;
+        public static readonly int HPRegenIncrease = 1;
+
+        public override void SetDefaults(Item item)
+        {
+            item.defense = 11;
+        }
+
+        public override void UpdateEquip(Item item, Player player)
+        {
+            player.statLifeMax2 += MaxHealthIncrease;
+            player.lifeRegen += HPRegenIncrease;
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            string tooltipText = Language.GetTextValue("Mods.MoreMulticlass.GlobalItems.PalladiumBreastplateOverride.Tooltip");
+
+            tooltips.Add(new TooltipLine(Mod, "Buffed",
+                string.Format(tooltipText, MaxHealthIncrease, HPRegenIncrease)));
+        }
 
 
     }
