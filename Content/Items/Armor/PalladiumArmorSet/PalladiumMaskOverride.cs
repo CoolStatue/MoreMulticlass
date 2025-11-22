1336fb6ad9aa08ae682f76e60d6b51acf0e94386
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2025-11-22 12:12:40 +0800

    PalladiumMaskOverride functionality

--- Content/Items/Armor/PalladiumArmorSet/PalladiumMaskOverride.cs
+++ Content/Items/Armor/PalladiumArmorSet/PalladiumMaskOverride.cs
@@ -11,6 +11,26 @@ namespace MoreMulticlass.Content.Items.Armor.PalladiumArmorSet
     public class PalladiumMaskOverride : GlobalItem
     {
         public override bool AppliesToEntity(Item entity, bool lateInstantiation)
-            => entity.type == ItemID.PalladiumMask; 
+            => entity.type == ItemID.PalladiumMask;
+
+         public static readonly int MaxHealthIncrease = 40;
+
+        public override void SetDefaults(Item item)
+        {
+            item.defense = 18;
+        }
+
+        public override void UpdateEquip(Item item, Player player)
+        {
+            player.statLifeMax2 += MaxHealthIncrease;
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            string tooltipText = Language.GetTextValue("Mods.MoreMulticlass.GlobalItems.PalladiumMaskOverride.Tooltip");
+
+            tooltips.Add(new TooltipLine(Mod, "Buffed",
+                string.Format(tooltipText, MaxHealthIncrease)));
+        }
     }
 }
