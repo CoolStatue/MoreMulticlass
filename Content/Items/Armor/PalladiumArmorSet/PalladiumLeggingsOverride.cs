7eac6f7fd4a1baf59822ad6d18738181d9f6aafe
CoolStatue <161408199+CoolStatue@users.noreply.github.com>
2025-11-22 09:01:30 +0800

    Palladium Armor Override boilerplate code

---
+++ Content/Items/Armor/PalladiumArmorSet/PalladiumLeggingsOverride.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using MoreMulticlass.Content.Players;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MoreMulticlass.Content.Items.Armor.PalladiumArmorSet
+{
+    public class PalladiumLeggingsOverride : GlobalItem
+    {
+        
+    }
+}
